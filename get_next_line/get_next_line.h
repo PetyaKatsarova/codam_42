85bed5ca1cad8b3fbc2da58d3bc2bbfa351d2f81
Petya Katsarova <pkatsaro@f0r3s13.codam.nl>
2023-03-14 09:05:47 +0100

    added completed project

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pkatsaro <pkatsaro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/19 14:12:30 by pkatsaro      #+#    #+#                 */
+/*   Updated: 2023/01/21 18:20:06 by pkatsaro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//The mos commonly used functions in <fcntl.h> include open(),
+// close(), and fcntl().
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_bonus.h                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pkatsaro <pkatsaro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/21 13:55:56 by pkatsaro      #+#    #+#                 */
+/*   Updated: 2023/01/21 18:25:01 by pkatsaro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+#endif
