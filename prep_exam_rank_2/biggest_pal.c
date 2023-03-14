4125b580bc72e036225befa5fa11dbcf273b7b86
Petya Katsarova <pkatsaro@f1r4s1.codam.nl>
2023-03-14 14:32:00 +0100

    working on biggest palindrone

---
+++ prep_exam_rank_2/biggest_pal.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void print_pal(char *str, int start, int end)
+{
+	while (start <= end)
+	{
+		write(1, &str[start], 1);
+		start++;
+	}
+	write(1, "\n", 1);
+}
+
+bool	is_pal(char *str, int start, int end)
+{
+	while (start < end)
+	{
+		if (str[start] != str[end])
+			return (false);
+		start++;
+		end--;
+	}
+	return (true); // true->is pal
+}
+
+void	biggest_pal(char *str)
+{
+	int	i, j, longest_start, longest_len;
+
+	i = 0;
+	j = ft_strlen(str);
+	longest_start = 0;
+	longest_len = 0;
+	// find the length and start of longest pal
+	while (i < j)
+	{
+		if (is_pal(str, i, j - 1))
+		{
+			if (j - i > longest_len)
+			{
+				longest_len = j - i;
+				longest_start = i;
+			}
+			j--;
+		}
+		i++;
+	}
+	print_pal(str, longest_start, longest_len + longest_start);
+}
+
+int	main (int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (1);
+	}
+	biggest_pal(argv[1]);
+	return (0);
+}
+
+/*
+Assignment name  : biggest_pal
+Expected files   : *.c, *.h
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program named biggest_pal that takes a string as argument and prints the
+biggest palindrome included inside this string, followed by a newline.
+
+This string contains only lowercase characters.
+
+If there are two palindromes of the same length, you have to print the last one.
+
+If there is more or less than 1 argument, your program must print only a newline
+only. ✓
+
+Exemples:
+
+$> biggest_pal "abcba"
+abcba
+$> biggest_pal "aaaaaaaaaabbcbbaaaa"
+aaaabbcbbaaaa
+$> biggest_pal "aooibdaoiwhoihwdoinzeldaisaboyobasiadlezfdsfnslk"
+zeldaisaboyobasiadlez
+$> biggest_pal "aeibaabaammaabaalek"
+aabaammaabaa
+$> biggest_pal abeb qpinqwjobo qkmnwoiq
+$> biggest_pal
+A palindrome is a word, phrase, number, or other sequence of characters that reads the
+same forward and backward. racecar
+$>
+
+*/
--- prep_exam_rank_2/union.c
+++ prep_exam_rank_2/union.c
@@ -49,11 +49,6 @@ int main(int argc, char	**argv)
 	return (0);
 }
 
-
-
-
-
-
 /*
 Assignment name  : union
 Expected files   : union.c
